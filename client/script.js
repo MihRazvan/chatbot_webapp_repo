d44ebb4447ba7bba42f6b75c1f3d3265fd58c289
Razvan Mihailescu <razvan.mihailescu1996@gmail.com>
2023-05-04 14:54:25 +0300

    clean the code + bug fixes

--- client/script.js
+++ client/script.js
@@ -1,5 +1,6 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import userIcon from './assets/user.svg';
+import botIcon from './assets/bot.svg';
+
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
@@ -39,37 +40,46 @@ function typeText(element, text, uniqueId) {
   }, 20)
 }
 
+function createChatStripe(isAi, value, uniqueId) {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  if (isAi) wrapper.classList.add('ai');
 
+  const chat = document.createElement('div');
+  chat.classList.add('chat');
 
-function chatStripe(isAi, value, uniqueId) {
-  return (
+  const profile = document.createElement('div');
+  profile.classList.add('profile');
 
-    `
-    <div class="wrapper ${isAi && 'ai'}">
-      <div class="chat">
-        <div class="profile">
-          <img src="${isAi ? bot : user}"/>
-        </div>
-        <div class="message" id=${uniqueId}>${value}</div>
-      </div>
-    </div>
-    `
-  )
+  const img = document.createElement('img');
+  img.setAttribute('src', isAi ? botIcon : userIcon);
+
+  const message = document.createElement('div');
+  message.classList.add('message');
+  message.setAttribute('id', uniqueId);
+  message.textContent = value;
+
+  profile.appendChild(img);
+  chat.appendChild(profile);
+  chat.appendChild(message);
+  wrapper.appendChild(chat);
+
+  return wrapper;
 }
 
-const handleSubmit = async (e) => {
+async function handleSubmit(e) {
   e.preventDefault();
 
   const data = new FormData(form);
 
   //user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.appendChild(createChatStripe(false, data.get('prompt')));
 
   form.reset();
 
   //bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.appendChild(createChatStripe(true, " ", uniqueId));
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
@@ -77,38 +87,41 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
+  try {
+    const response = await fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt')
+      })
+    });
 
-  clearInterval(loadInterval)
-  messageDiv.innerHTML = " "
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
 
+    if (response.ok) {
+      const { bot } = await response.json();
+      const parsedData = bot.trim();
 
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+      typeText(messageDiv, parsedData, uniqueId);
+    } else {
+      const err = await response.text()
 
-    typeText(messageDiv, parsedData, uniqueId)
-  } else {
-    const err = await response.text()
-
-    messageDiv.innerHTML = "Something went wrong"
-    alert(err)
+      messageDiv.innerHTML = "Something went wrong";
+      alert(err);
+    }
+  } catch (error) {
+    console.error(error);
+    messageDiv.innerHTML = "Something went wrong";
+    alert(error);
   }
-
 }
-
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
-})
+});
